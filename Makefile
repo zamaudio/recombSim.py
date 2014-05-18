4848e6349e838e6013f0e6f36f01ad4571d4f3f5
Damien Zammit <damien@zamaudio.com>
2014-05-18 18:35:34 +1000

    Added licence preambles
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
+# This file is part of recombsim
+
 all: Makefile mtrand.cpp mtrand.h recombsim.cpp 
 	g++ -O3 mtrand.cpp recombsim.cpp -lbpp-core -lbpp-seq -g -o recombsim
 
 clean:
-	rm recombsim *.o *~
+	rm recombsim *~
--- recombsim.cpp
+++ recombsim.cpp
@@ -1,3 +1,21 @@
+/*
+ * recombsim.cpp  Recombination in bacteria simulation data generator
+ * Copyright (C) 2014  Damien Zammit <damien@zamaudio.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For a full copy of the GNU General Public License
+ * see <http://www.gnu.org/licenses/>.
+ */
+
 #include <cstdio>
 #include "recombsim.h"
 #include "mtrand.h"
